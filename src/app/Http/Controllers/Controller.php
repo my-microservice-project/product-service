44bc4db97083ae52512b69d87ba7eb5088228199
abfbilisim <bugrabozkurtt@gmail.com>
2025-02-08 00:51:17 +0300

    Abstract controller annotate set with attribute

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -3,22 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Traits\ResponseTrait;
-use OpenApi\Annotations as OA;
+use OpenApi\Attributes as OA;
 
-/**
- * @OA\Info(
- *     title="Product Service",
- *     version="1.0.0",
- *     description="Product API Documentation",
- *     @OA\Contact(
- *         email="bugrabozkurtt@gmail.com"
- *     ),
- *     @OA\License(
- *         name="MIT",
- *         url="https://opensource.org/licenses/MIT"
- *     )
- * )
- */
+#[OA\Info(
+    version: "1.0.0",
+    description: "Product API Documentation",
+    title: "Product Service",
+    contact: new OA\Contact(email: "bugrabozkurtt@gmail.com"),
+    license: new OA\License(name: "MIT", url: "https://opensource.org/licenses/MIT")
+)]
 abstract class Controller
 {
     use ResponseTrait;
